551a2308fa8ae7684881e58bfecfb9d9cb7a6d33
Alejandro Fontal <alejandrofontal92@gmail.com>
2022-11-27 16:00:53 +0100

    fix: adapt to new site changes, py3.10 selenium API (#3)

--- japan_aqi.py
+++ japan_aqi.py
@@ -8,6 +8,7 @@ from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
 
 load_dotenv()
 geckodriver_autoinstaller.install()
@@ -25,7 +26,7 @@ download_folder = 'tmp_downloads'
 data_folder = 'data/japan-aqi'
 
 if os.path.exists(download_folder):
-    os.rmdir(download_folder)
+    shutil.rmtree(download_folder)
 os.mkdir(download_folder)
 page = requests.get(japan_stations_url)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -36,36 +37,43 @@ for station_link in soup.find(id='map-stations').find_all('a'):
 
 options = webdriver.FirefoxOptions()
 options.headless = True
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", os.path.join(os.getcwd(), download_folder))
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain")
-driver = webdriver.Firefox(firefox_profile=profile, options=options)
+options.set_preference("browser.download.folderList", 2)
+options.set_preference("browser.download.manager.showWhenStarting", False)
+options.set_preference("browser.download.dir",
+                       os.path.join(os.getcwd(), download_folder))
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain")
+driver = webdriver.Firefox(options=options)
 
 
 def download_station_data(driver: webdriver, station: str, sleep_time : int = 1):
+    w, h = driver.get_window_size().values()
     station_without_non_ascii = station.encode("ascii", "ignore").decode()
     driver.get('https://aqicn.org/data-platform/register/')
-    historic_div = driver.find_element_by_id('historic-aqidata')
+    historic_div = driver.find_element(By.ID, 'historic-aqidata')
     driver.execute_script("return arguments[0].scrollIntoView();", historic_div)
-    driver.execute_script("window.scrollBy(0, -150);")
-    station_prompt = historic_div.find_element_by_class_name('prompt')
+    driver.execute_script(f"window.scrollBy(0, -{h/2});")
+    station_prompt = historic_div.find_element(By.CLASS_NAME,'prompt')
     station_prompt.send_keys(station_without_non_ascii)
     sleep(sleep_time)
-    search_result = historic_div.find_element_by_class_name('result')
+    search_result = historic_div.find_element(By.CLASS_NAME, 'result')
+    driver.execute_script("return arguments[0].scrollIntoView();", search_result)
+    driver.execute_script(f"window.scrollBy(0, -{h/2});")
     search_result.click()
-    sleep(sleep_time)
-    download_button = driver.find_element_by_class_name('ui.large.primary.button')
+    sleep(sleep_time) 
+    download_button = driver.find_element(By.CLASS_NAME, 'ui.large.primary.button')
     download_button.click()
     sleep(sleep_time * 2)
-    three_inputs = driver.find_element_by_class_name('three.fields')
-    driver.execute_script("return arguments[0].scrollIntoView();", three_inputs)
-    three_inputs.find_element_by_name('name').send_keys(my_name)
-    three_inputs.find_element_by_name('email').send_keys(my_email)
-    three_inputs.find_element_by_name('organization').send_keys(my_org)
-    driver.find_element_by_name('terms').click()
-    submit_button = driver.find_element_by_class_name('ui.submit.button.primary')
+    three_inputs = driver.find_element(By.CLASS_NAME, 'three.fields')
+    three_inputs.find_element(By.NAME, 'name').send_keys(my_name)
+    three_inputs.find_element(By.NAME, 'email').send_keys(my_email)
+    three_inputs.find_element(By.NAME, 'organization').send_keys(my_org)
+    terms = driver.find_element(By.NAME, 'terms')
+    driver.execute_script("return arguments[0].scrollIntoView();", terms)
+    driver.execute_script(f"window.scrollBy(0, -{h/2});")
+    terms.click()    
+    submit_button = driver.find_element(By.CLASS_NAME, 'ui.submit.button.primary')
+    driver.execute_script("return arguments[0].scrollIntoView();", submit_button)
+    driver.execute_script(f"window.scrollBy(0, -{h/2});")
     submit_button.click()
     sleep(sleep_time * 3)
 
@@ -75,7 +83,9 @@ for station in stations_list:
         download_station_data(driver, station, sleep_time=2)
         logger.info(msg=f'{station} data correctly downloaded')
     except Exception as e:
-        logger.info(msg=f"{station} data couldn't be fetched: {e}, retrying with higher wait time")
+        logger.info(msg=
+        f"{station} data couldn't be fetched: {e}, retrying with higher wait time"
+        )
         try:
             download_station_data(driver, station, sleep_time=4)
             logger.info(msg=f'{station} data correctly downloaded')
